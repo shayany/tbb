24200e44ebd56632be7d8d0d7c4087586a851c9c
Shayan Yazdanmehr <shayany@ifi.uio.no>
2015-11-03 19:33:06 +0100

    serial matrix multipication

---
+++ matrix_multipication/serial/multipication.cpp
@@ -0,0 +1,88 @@
+#include <iomanip>
+#include <stdlib.h>
+#include <iostream>
+#include <tbb/tbb.h>
+#include <cmath>
+#include <omp.h>
+#include <vector>
+
+int const n=500;
+int const m=500;
+int const size=500;
+
+void calculate(float a[n][size],float b[size][m],float c[n][m]){    
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            for(int k=0;k<size;k++)
+            {
+                c[i][j]+=a[i][k]*b[k][j];
+            }                
+        }
+    }
+
+}
+void main()
+{
+    float a[n][size];
+    float b[size][m];
+    float c[n][m];
+    double timer=0.;    
+
+    float counter=1;
+    
+    for (int i=0; i<n; i++)
+        for (int j=0; j<size; j++)
+            a[i][j]=counter++;
+    
+    for (int i=0; i<size; i++)
+        for (int j=0; j<m; j++)
+            b[i][j]=counter++;
+
+    for (int i=0; i<n; i++)
+        for (int j=0; j<m; j++)
+            c[i][j]=0;    
+    
+    timer-=omp_get_wtime();//Start timer
+    
+    calculate(a,b,c);
+
+    timer+=omp_get_wtime();//End timer
+    
+    
+    std::cout.setf(std::ios::fixed);
+    std::cout<<"\nTime:"<<std::setprecision(15)<<timer<<"\n";
+    /*
+    std::cout.setf(std::ios::fixed);
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<size; j++)
+        {
+            std::cout<<std::setprecision(2)<<a[i][j]<<"\t";
+        }    
+        std::cout<<"\n";
+    }
+    std::cout.setf(std::ios::fixed);
+    std::cout<<"\n";
+    for (int i=0; i<size; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            std::cout<<std::setprecision(2)<<b[i][j]<<"\t";
+        }    
+        std::cout<<"\n";
+    }
+    std::cout<<"\n";
+    std::cout.setf(std::ios::fixed);
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            std::cout<<std::setprecision(2)<<c[i][j]<<"\t";
+        }    
+        std::cout<<"\n";
+    }*/
+}
+
